750be0bbb0116dea1d3d2adfdc8f4c0e5bd479cb
Beldon <1047325205@qq.com>
2016-02-02 23:07:59 +0800

    fix bug

--- core/src/main/java/belog/service/impl/TermTaxonomyServiceImpl.java
+++ core/src/main/java/belog/service/impl/TermTaxonomyServiceImpl.java
@@ -1,12 +1,16 @@
 package belog.service.impl;
 
+import belog.dao.TermRelationshipsDao;
 import belog.dao.TermTaxonomyDao;
 import belog.dao.TermsDao;
+import belog.pojo.po.TermRelationships;
 import belog.pojo.po.TermTaxonomy;
 import belog.pojo.po.Terms;
 import belog.service.TermTaxonomyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Set;
 
 /**
  * Created by Beldon
@@ -19,6 +23,9 @@ public class TermTaxonomyServiceImpl extends BaseService implements TermTaxonomy
 
     @Autowired
     private TermsDao termsDao;
+
+    @Autowired
+    private TermRelationshipsDao termRelationshipsDao;
 
     public void countPlus(long catId, long sum) {
         TermTaxonomy taxonomy = termTaxonomyDao.findById(catId);
@@ -39,6 +46,11 @@ public class TermTaxonomyServiceImpl extends BaseService implements TermTaxonomy
     public void deleteById(long id) {
         TermTaxonomy termTaxonomy = termTaxonomyDao.findById(id);
         Terms terms = termTaxonomy.getTerms();
+        Set<TermRelationships> relationshipses = termTaxonomy.getTermRelationships();
+        if (relationshipses != null && relationshipses.size() > 0) {
+            termRelationshipsDao.deleteBatch(relationshipses);
+        }
+
         termTaxonomyDao.deleteEntity(termTaxonomy);
         termsDao.deleteEntity(terms);
     }
